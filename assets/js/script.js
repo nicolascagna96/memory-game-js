267c6d93c1fafdc6f75ba638f76924a02ed5b223
nicolascagna96 <nicolascagna@hotmail.com>
2022-05-09 07:46:51 +0000

    Adding the logic to our game

--- assets/js/script.js
+++ assets/js/script.js
@@ -24,3 +24,39 @@ const getData = () => [
     { imgSrc: "assets/images/img-7.png", name:"card7"},
     { imgSrc: "assets/images/img-8.png", name:"card8"},
 ];
+
+//Randomize array
+
+const randomize = () => {
+    const cardData = getData();
+    cardData.sort(() => Math.random() - 0.5);
+    return cardData;
+};
+
+cardGenerator = () => {
+    const cardData = randomize();
+
+//Genearte HTML in loop
+cardData.forEach((item, index) => {
+        const card = document.createElement('div');
+        const face = document.createElement('img');
+        const back = document.createElement('div');
+        card.classList = "card";
+        face.classList = 'face';
+        back.classList = 'back';
+
+        //Attach info to the cards
+        face.src = item.imgSrc;
+        card.setAttribute('name', item.name);
+        section.appendChild(card);
+        card.appendChild(face);
+        card.appendChild(back);
+
+        card.addEventListener('click', (e) => {
+        card.classList.toggle('toggleCard');
+        checkCards(e);
+
+        })
+    });
+};
+
